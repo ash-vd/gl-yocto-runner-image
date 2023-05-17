e5988074b0abf57af3ca6699b3d1429ce6c9a50d
Ashwin <contact@ashwinvandijk.nl>
2023-05-17 14:30:39 +0200

    envs

--- start.sh
+++ start.sh
@@ -2,19 +2,19 @@
 ORGANIZATION=$ORGANIZATION
 ACCESS_TOKEN=$ACCESS_TOKEN
 
-REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
+REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" "https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token" | jq .token --raw-output)
 
-echo ORGANIZATION
+echo "ORGANIZATION"
 echo ACCESS_TOKEN
-echo REG_TOKEN
+echo "${REG_TOKEN}"
 
 cd /home/docker/actions-runner
 
-./config.sh --url https://github.com/${ORGANIZATION} --token ${REG_TOKEN} --labels yocto,x64,linux
+./config.sh --url "https://github.com/${ORGANIZATION}" --token ${REG_TOKEN} --labels yocto,x64,linux
 
 cleanup() {
     echo "Removing runner..."
-    ./config.sh remove --unattended --token ${REG_TOKEN}
+    ./config.sh remove --unattended --token "${REG_TOKEN}"
 }
 
 trap 'cleanup; exit 130' INT
