0d7493c6694356c87d060a8a7af7a248d3fbb515
Ashwin <contact@ashwinvandijk.nl>
2023-05-17 13:53:00 +0200

    add printenv

--- start.sh
+++ start.sh
@@ -2,8 +2,11 @@
 ORGANIZATION=$ORGANIZATION
 ACCESS_TOKEN=$ACCESS_TOKEN
 
+printenv
+
 echo $ORGANIZATION
 echo $ACCESS_TOKEN
+
 
 REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
 
